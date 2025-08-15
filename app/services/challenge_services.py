9cb8ca3cdf3c5c656857886032d58b2a45c5a670
asileshi <abebesileshi828@gmail.com>
2025-08-15 23:58:38 +0300

    challenge services completed

--- app/services/challenge_services.py
+++ app/services/challenge_services.py
@@ -4,15 +4,30 @@ from fastapi import HTTPException
 from app.models.challenges import Challenge
 from app.models.skills import Skill
 from app.models.user import User
-from app.schemas.challenge import ChallengeCreate, ChallengeUpdate
+from app.schemas.challenge import ChallengeCreate
 from typing import List
 
-def create_challenge(db: Session, challenge_data: ChallengeCreate, current_user: User) -> Challenge:
-    skill = db.query(Skill).filter(
-        Skill.id == challenge_data.skill_id,
-        Skill.owner_id == current_user.id
-        ).first()
-    
+def create_challenge(db: Session, challenge_data: ChallengeCreate) -> Challenge:
+    skill = db.query(Skill).filter(Skill.id == challenge_data.skill_id).first()
     if not skill:
-        raise HTTPException(status_code=404, detail="Skill not found or not owned by user")
-    
+        raise HTTPException(status_code=404, detail="Skill not found")
+    challenge = Challenge(
+        title=challenge_data.title,
+        description=challenge_data.description,
+        difficulty=challenge_data.difficulty,
+        skill_id=challenge_data.skill_id
+    )
+    db.add(challenge)
+    db.commit()
+    db.refresh(challenge)
+    return challenge
+
+def get_all_challenges(db: Session) -> List[Challenge]:
+    challenges = db.query(Challenge).all()
+    return challenges
+
+def get_challenge_by_id(db: Session, challenge_id: int) -> Challenge:
+    challenge = db.query(Challenge).filter(Challenge.id == challenge_id).first()
+    if not challenge:
+        raise HTTPException(status_code=404, detail="Challenge not found")
+    return challenge
