f8ccf16ea8f3ef76e83819bdcc148152c92be139
asileshi <abebesileshi828@gmail.com>
2025-08-22 21:21:29 +0300

    Admit routes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from app.db.session import engine, Base
-from app.routes import auth, skill  
+from app.routes import auth, skill, challenge 
 from app.models import skills, user, challenges
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(skill.router)
+app.include_router(challenge.router)
 Base.metadata.create_all(bind=engine)
